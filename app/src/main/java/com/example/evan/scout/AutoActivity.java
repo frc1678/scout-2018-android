34439af3fd999fc2356207270accd61cb094bd66
Iyatsu <cwhuang2002@icloud.com>
2018-01-17 20:40:59 -0800

    Same merge as on Saturday, don't know why the one on Saturday didn't work. (#28)
    * updated Time in all the UIcomponentcreator sublcasses
    
    * FINAL COMMIT - Fixed universal stuff for future edits in Data
    
    * Master FIX, 1/17/18, previous pull request didn't update the master for some reason

--- app/src/main/java/com/example/evan/scout/AutoActivity.java
+++ app/src/main/java/com/example/evan/scout/AutoActivity.java
@@ -6,17 +6,20 @@ import java.util.List;
 
 public class AutoActivity extends DataActivity {
     public String activityName(){   return "auto"; }
-    public List<String> getShotData() {
-        return formatList(Arrays.asList("", ""));
-    }
     public List<String> getToggleData() {
         return null;
     }
     public List<String> getCounterData() {
-        return formatList(Arrays.asList(""));
+        return null;
     }
+    public List<String> getSwitchData() { return null; }
+    public List<String> getScaleData() { return null; }
+    public List<String> getPyramidData() { return null; }
     public Integer getToggleXML() { return null; }
     public Integer getCounterXML() { return null; }
+    public Integer getSwitchXML() { return null; }
+    public Integer getScaleXML() { return null; }
+    public Integer getPyramidXML() { return null; }
 
     @Override
     public Class getNextActivityClass() {return TeleopActivity.class;}
