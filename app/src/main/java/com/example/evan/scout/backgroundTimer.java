88101d5a0ffd472c9ed6198045b34d1c49cf3001
Iyatsu <cwhuang2002@icloud.com>
2018-02-21 20:22:43 -0800

    Issue97 (#99)
    * teamnumber, matchnumber, and alliancecolor, are fluidly and correctly implemented with overridden and firebase situations
    
    * issue 97 fixed, timer resets

--- app/src/main/java/com/example/evan/scout/backgroundTimer.java
+++ app/src/main/java/com/example/evan/scout/backgroundTimer.java
@@ -21,6 +21,7 @@ public class backgroundTimer extends Thread{
     public boolean timerReady = true;
     public int showTime;
     public static float updatedTime;
+    public static boolean stopTimer = false;
     public CountDownTimer matchTimer;
     Activity context;
 
@@ -34,15 +35,20 @@ public class backgroundTimer extends Thread{
     private void startTimer(){
         Log.e("TIMERCALLED","CALLED START TIMER!!!!!");
         updatedTime = 0f;
+        stopTimer = false;
         matchTimer = new CountDownTimer(15000, 10) {
             @Override
             public void onTick(long millisUntilFinished) {
-                float MUF = millisUntilFinished;
-                updatedTime = (15000f - MUF)/1000f;
-                updatedTime = Float.parseFloat(String.format("%.2f", updatedTime));
-                showTime = (int) updatedTime;
-                MenuItem timerView = currentMenu.findItem(R.id.timerView);
-                timerView.setTitle("AutoTime: "+showTime+" / 15");
+                if(stopTimer){
+                    matchTimer.cancel();
+                }else{
+                    float MUF = millisUntilFinished;
+                    updatedTime = (15000f - MUF)/1000f;
+                    updatedTime = Float.parseFloat(String.format("%.2f", updatedTime));
+                    showTime = (int) updatedTime;
+                    MenuItem timerView = currentMenu.findItem(R.id.timerView);
+                    timerView.setTitle("AutoTime: "+showTime+" / 15");
+                }
             }
 
             @Override
@@ -58,12 +64,16 @@ public class backgroundTimer extends Thread{
         matchTimer = new CountDownTimer(135000, 10) {
             @Override
             public void onTick(long millisUntilFinished) {
-                float MUF = millisUntilFinished;
-                updatedTime = (135000f - MUF)/1000f;
-                updatedTime = Float.parseFloat(String.format("%.2f", updatedTime));
-                showTime = (int) updatedTime;
-                MenuItem timerView = currentMenu.findItem(R.id.timerView);
-                timerView.setTitle("TeleTime: "+showTime+" / 135");
+                if(stopTimer){
+                    matchTimer.cancel();
+                }else {
+                    float MUF = millisUntilFinished;
+                    updatedTime = (135000f - MUF)/1000f;
+                    updatedTime = Float.parseFloat(String.format("%.2f", updatedTime));
+                    showTime = (int) updatedTime;
+                    MenuItem timerView = currentMenu.findItem(R.id.timerView);
+                    timerView.setTitle("TeleTime: "+showTime+" / 135");
+                }
             }
 
             @Override
@@ -76,4 +86,5 @@ public class backgroundTimer extends Thread{
     public static float getUpdatedTime(){
         return updatedTime;
     }
+    public static void stopTimer(){        stopTimer = true; updatedTime = 0f;}
 }
